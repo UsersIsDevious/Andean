67012edded29ce701f5e41e4e77f2a3ddfd306f8
GameAndCo <system@gameandco.co.jp>
2024-09-27 04:04:24 +0900

    add Node sse

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+REM スクリプトがあるディレクトリに移動
+cd /d "%~dp0"
+
+start http://localhost:3000/
+start http://localhost:3000/observer.html
+
+REM binフォルダ内のnode.exeを使用してserver.jsを実行
+bin\node.exe server.js
+
