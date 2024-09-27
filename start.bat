ace6c817419e4db5ae8e4e29063234cabc6272c8
conecone <conecone7234@gmail.com>
2024-09-28 03:54:39 +0900

    Add Node.js

--- start.bat
+++ start.bat
@@ -1,1 +1,6 @@
-Scripts\python.exe liveapi_server.py
+@echo off
+set PATH=%PATH%;%cd%
+set NODE_PATH=%cd%\node-v20.17.0-win-x64\node_modules\npm\node_modules;%cd%\node-v20.17.0-win-x64\node_modules\npm
+cmd
+
+@REM Scripts\python.exe liveapi_server.py
