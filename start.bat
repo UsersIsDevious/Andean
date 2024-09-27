52fc7df1da01eba4ef9603af18f34b6d3619e536
conecone <conecone7234@gmail.com>
2024-09-28 01:00:42 +0900

    Clean repository

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM スクリプトがあるディレクトリに移動
-cd /d "%~dp0"
-
-start http://localhost:3000/view
-start http://localhost:3000/controller
-
-REM binフォルダ内のnode.exeを使用してserver.jsを実行
-bin\node.exe src\server.js
