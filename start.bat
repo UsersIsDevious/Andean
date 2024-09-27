efa1e0935db884488e4d6cc7b85001885410694b
GameAndCo <system@gameandco.co.jp>
2024-09-27 18:28:39 +0900

    Changing the directory configuration

--- start.bat
+++ start.bat
@@ -2,8 +2,8 @@
 REM スクリプトがあるディレクトリに移動
 cd /d "%~dp0"
 
-start http://localhost:3000/
-start http://localhost:3000/observer.html
+start http://localhost:3000/view
+start http://localhost:3000/controller
 
 REM binフォルダ内のnode.exeを使用してserver.jsを実行
 bin\node.exe src\server.js
