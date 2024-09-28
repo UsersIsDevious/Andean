16fdc8088778bb94ae02ca2b7bfa97e9b847382f
conecone <conecone7234@gmail.com>
2024-09-28 18:49:17 +0900

    First commit

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=%PATH%;%cd%
-set NODE_PATH=%cd%\node-v20.17.0-win-x64\node_modules\npm\node_modules;%cd%\node-v20.17.0-win-x64\node_modules\npm
-cmd
-
-@REM Scripts\python.exe liveapi_server.py
