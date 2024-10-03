f531977914f0427c75ec226923309d111854f425
conecone <conecone7234@gmail.com>
2024-10-03 21:39:36 +0900

    Add create_lobby Part2

--- src/apexCommon.js
+++ src/apexCommon.js
@@ -1,11 +1,23 @@
-
+const { 
+    Request,
+    CustomMatch_CreateLobby 
+} = require('../bin/events_pb'); // Requestをインポート
 
 function hoge(e){
     return ""
 }
 
-function create_lobby(req){
-    req.CustomMatch_CreateLobby;
+function create_lobby() {
+    const req = new Request();  // Requestオブジェクトを作成
+    const createLobby = new CustomMatch_CreateLobby();
+
+    req.setCustommatchCreatelobby(createLobby);
+    req.setWithack(true);  // ack（確認応答）を要求
+
+    const serialized = req.serializeBinary();
+    wss.send(serialized);  // シリアライズされたリクエストをWebSocket経由で送信
+
+    console.log("Do IT !!!!");  // 動作テスト用
 }
 
 // モジュールとしてエクスポート
