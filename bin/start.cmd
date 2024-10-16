f451b8269981408b75f36e11ac2dc57c2bd50097
conecone <conecone7234@gmail.com>
2024-10-16 22:59:44 +0900

    Fix bin/start.cmd

--- bin/start.cmd
+++ bin/start.cmd
@@ -4,4 +4,3 @@ chcp 65001
 set PATH=%PATH%;%cd%\node-v20.17.0-win-x64
 set NODE_PATH=%cd%\node-v20.17.0-win-x64\node_modules\npm\node_modules;%cd%\node-v20.17.0-win-x64\node_modules\npm
 set PROTOC=%cd%\protoc-28.2-win64\bin
-node start.js
