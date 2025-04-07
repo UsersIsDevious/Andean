4403900b16aa47d9b4d2ad7ae1da36877e32149a
Nick-Sugar <nicksugar0141@gmail.com>
2025-04-08 00:44:51 +0900

    live-view-1

---
+++ Andean/AndeanWebUI/Services/LiveViewHubService.cs
@@ -0,0 +1,25 @@
+﻿using AndeanSystems;
+using AndeanWebUI.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using AndeanClass.Controllers;
+using System.Threading.Tasks;
+
+namespace AndeanWebUI.Services
+{
+    public class LiveViewHubUpdateService : AndeanSystem
+    {
+        private readonly IHubContext<LiveViewHub> _hubContext;
+
+        public LiveViewHubUpdateService(IHubContext<LiveViewHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        // この関数は60FPS(約16ms毎)で呼ばれると仮定
+        public override async void Update()
+        {
+            // マッチ情報をクライアントに送信
+            await _hubContext.Clients.All.SendAsync("ReceiveMatchData", AndeanClassController._match);
+        }
+    }
+}
